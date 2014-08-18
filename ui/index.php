a090a90ce04d060213c0cfb2100d687b7b79d493
perrie <jssc.perrie@gmail.com>
2014-08-17 21:40:25 -0400

    updated for better support for different file presentation/logic

--- ui/index.php
+++ ui/index.php
@@ -1,13 +1,18 @@
 <?php
 
+include ("inc/logic/clean_get.inc.php");
 $page_title = "Search";
-include("inc/head1.inc.php");
-include("inc/head2.inc.php");
-include("inc/connect.inc.php");
+include("inc/html/head_open.inc.php");
+include("inc/html/head_close.inc.php");
 
-include("inc/search.inc.php");
-include("inc/search_results.inc.php");
 
-include("inc/close.inc.php");
+include("inc/logic/connect.inc.php");
+
+include("inc/logic/search_params.inc.php");
+include("inc/html/search.inc.php");
+include("inc/logic/search_process.inc.php");
+include("inc/html/search_results.inc.php");
+
+include("inc/html/close.inc.php");
 
 ?>
